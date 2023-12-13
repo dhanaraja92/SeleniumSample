bfa1527c5e20e09818fe58236cb164a424e85cbf
dhanaraja92 <dhanaraja.dc@gmail.com>
2023-12-13 12:41:24 +0530

    13.12.2023

---
+++ src/test/java/org/facebook/Facebook.java
@@ -0,0 +1,15 @@
+package org.facebook;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Facebook {
+
+	@Test
+	public static void testMethod() {
+		WebDriver driver = new ChromeDriver();
+
+	}
+
+}
